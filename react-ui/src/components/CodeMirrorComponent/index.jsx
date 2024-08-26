76c33d9e4e15d25ee0375dc8b0e3febaa84a7529
Dora Do <doradocodes@gmail.com>
2024-08-25 23:25:25 -0400

    Update index.jsx

--- react-ui/src/components/CodeMirrorComponent/index.jsx
+++ react-ui/src/components/CodeMirrorComponent/index.jsx
@@ -1,35 +1,74 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
+import CodeMirror, {Decoration, WidgetType} from '@uiw/react-codemirror';
 
 import {basicSetup, EditorView} from "codemirror"
 import {java} from "@codemirror/lang-java";
 import styles from './index.module.css';
 import {useEditorStore} from "../../store/editorStore.js";
+import {collab, getSyncedVersion, receiveUpdates, sendableUpdates} from "@codemirror/collab";
+import {ChangeSet, EditorState, RangeSetBuilder} from '@codemirror/state';
+import {socket} from "../../socket.js";
 
-let editor = null;
+const RemoteCursorWidget = () => {
+    const span = document.createElement("span");
+    span.className = "remote-cursor";
+    span.style.borderLeft = "2px solid red";  // Customize cursor appearance
+    span.style.height = "1em";
+    span.style.marginLeft = "-1px";  // Aligns with text cursor
+    return span;
+};
 
 const CodeMirrorComponent = () => {
-    const [editorHeight, setEditorHeight] = useState('100%');
-    const updateEditorContent = useEditorStore(state => state.updateContent);
+    const [value, setValue] = React.useState("test");
+    const editorRef = useRef(null);
+    const [initState, setInitState] = useState(null);
+    const [cursorPosition, setCursorPosition] = useState(null);
 
     useEffect(() => {
         console.log('CodeMirrorComponent loaded');
 
-        editor = new EditorView({
-            // doc: testSketch,
-            extensions: [
-                basicSetup,
-                java(),
-                EditorView.updateListener.of(update => {
-                    if (update.docChanged) {
-                        // Content of the editor has changed
-                        console.log("Editor content changed!");
-                        // You can add your custom logic here
-                        updateEditorContent(update.state.doc.toString());
-                    }
-                })
-            ],
-            parent: document.body.querySelector('#editor')
+        socket.on('update', (updateObj) => {
+            console.log('received update', updateObj);
+            // let received = updateObj.updates.slice(-1).map(json => ({
+            //     clientID: json.clientID,
+            //     changes: ChangeSet.fromJSON(json.changes)
+            // }))
+            // console.log('received', received);
+            // const viewUpdate = receiveUpdates(editorRef.current.view.state, received);
+            // editorRef.current.view.update([viewUpdate]);
 
+            setCursorPosition(updateObj.cursorPosition);
+        });
+
+        setCursorPosition();
+    }, []);
+
+    const createRemoteCursorDecorations = (remoteCursor) => {
+        const builder = new RangeSetBuilder();
+        // const pos = remoteCursor.ranges[0].head;
+        const cursor = Decoration.widget({
+            widget: new class extends WidgetType {
+                toDOM() {
+                    return RemoteCursorWidget();
+                }
+            },
+            side: 1
+        }).range(1);
+        builder.add(1, 1, cursor);
+        return builder.finish();
+    };
+
+    const onChange = useCallback((val, viewUpdate) => {
+        const updates = sendableUpdates(viewUpdate.state);
+        const version = getSyncedVersion(viewUpdate.state);
+        console.log('sending update', updates, version);
+        const obj = {
+            updates: updates,
+            version: version,
+            cursorPosition: viewUpdate.state.selection
+        };
+        socket.emit('update', obj, () => {
+            console.log('sent update', obj);
         });
 
 
@@ -37,11 +76,23 @@ const CodeMirrorComponent = () => {
         // setEditorHeight(editorHeight);
     }, []);
 
-    return <div className={styles.wrapper}>
-        <div id="editor" className={styles.editor} style={{ maxHeight: editorHeight }}>
-            <div className={styles.editorGutter}></div>
-        </div>
-    </div>
-}
+    // if (cursorPosition) {
+    //     extensions.push();
+    // }
+
+    return <CodeMirror
+        ref={editorRef}
+        value={value}
+        height="100%"
+        extensions={[
+            java(),
+            collab({
+                startVersion: 1
+            }),
+            // EditorView.decorations.of(createRemoteCursorDecorations())
+        ]}
+        onChange={onChange}
+    />;
+};
 
 export default CodeMirrorComponent;
