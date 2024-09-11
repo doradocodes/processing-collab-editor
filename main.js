2fce162a81d7f33b180db53afa0b5871efc444b3
Dora Do <doradocodes@gmail.com>
2024-09-10 20:18:03 -0700

    Add electron functions for creating/saving local sketches

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// main.js
-
-// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 const { exec } = require('child_process')
 const { ipcMain } = require('electron')
 const fs = require('node:fs');
-
 
 const createWindow = () => {
     // Create the browser window.
@@ -17,13 +13,40 @@ const createWindow = () => {
             preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
             contextIsolation: true,
-        }
+        },
+        titleBarStyle: 'hidden',
     })
     // Load your React app in production mode
     const startUrl = process.env.ELECTRON_START_URL || path.join(__dirname, '../build/index.html');
     mainWindow.loadURL(startUrl);
 
     mainWindow.webContents.openDevTools();
+
+    const secondaryWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: true,
+        },
+        titleBarStyle: 'hidden',
+    });
+    secondaryWindow.loadURL(startUrl);
+    secondaryWindow.webContents.openDevTools();
+    //
+    // const thirdWindow = new BrowserWindow({
+    //     width: 800,
+    //     height: 600,
+    //     webPreferences: {
+    //         preload: path.join(__dirname, 'preload.js'),
+    //         nodeIntegration: true,
+    //         contextIsolation: true,
+    //     }
+    // });
+    // thirdWindow.loadURL(startUrl);
+    // thirdWindow.webContents.openDevTools();
+
 }
 
 // This method will be called when Electron has finished
@@ -46,12 +69,9 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
 
 const createSketchFile = (fileName, fileContent) => {
     const folderName = fileName;
-    // const folderName = 'test_sketch5';
     const folderPath = path.join(__dirname + '/sketches', folderName);
     const filePath = path.join(folderPath, folderName + '.pde');
     return new Promise ((resolve, reject) => {
@@ -62,56 +82,97 @@ const createSketchFile = (fileName, fileContent) => {
         } catch (error) {
             reject(error);
         }
-
-        // fs.mkdir(folderPath, { recursive: true }, (err) => {
-        //     if (err) {
-        //         console.error(`Error creating folder: ${err.message}`);
-        //         reject(err);
-        //     }
-        //
-        //     console.log('Folder created successfully.');
-        //
-        //     // Write the file
-        //     fs.writeFile(filePath, fileContent, { encoding: 'utf8' }, (err) => {
-        //         if (err) {
-        //             console.error(`Error writing file: ${err.message}`);
-        //             reject(err);
-        //         }
-        //
-        //         console.log('File written successfully.');
-        //         resolve(folderPath);
-        //     });
-        // });
     });
 }
 
+const getSketchFolders = () => {
+    const sketchesPath = path.join(__dirname, 'sketches');
+    return new Promise((resolve, reject) => {
+        fs.readdir(sketchesPath, { withFileTypes: true }, (err, entries) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            const folders = entries
+                .filter(entry => entry.isDirectory())
+                .map(dir => dir.name);
+            resolve(folders);
+        });
+    });
+};
+
+const getSketchFile = (folderName) => {
+    const folderPath = path.join(__dirname, 'sketches', folderName);
+    const filePath = path.join(folderPath, folderName + '.pde');
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+}
+
+ipcMain.handle('get-sketch-folders', async (event) => {
+    try {
+        const folders = await getSketchFolders();
+        return folders;
+    } catch (error) {
+        console.error('Error getting sketch folders:', error);
+        throw error;
+    }
+});
+
+ipcMain.handle('get-sketch-file', async (event, folderName) => {
+    try {
+        const fileContent = await getSketchFile(folderName);
+        return fileContent;
+    } catch (error) {
+        console.error('Error getting sketch file:', error);
+        throw error;
+    }
+});
+
+ipcMain.handle('create-new-sketch', async (event, fileName, content) => {
+    try {
+        const folderPath = await createSketchFile(fileName, content);
+        console.log('Sketch created successfully', folderPath);
+        return folderPath;
+    } catch (error) {
+        console.error('Error creating sketch:', error);
+        throw error
+    }
+});
+
 ipcMain.handle('run-processing', (event, fileName, content) => {
     return new Promise((resolve, reject) => {
-        createSketchFile(fileName, content).then((folderPath) => {
-            console.log('Sketch created successfully', folderPath);
-
-            const process = exec(`processing-java --sketch=${folderPath} --run`);
-
-            process.stdout.on('data', (data) => {
-                console.log('stdout:', data.toString());
-                event.sender.send('processing-output', data.toString());
-            });
-
-            process.stderr.on('data', (data) => {
-                console.error('stderr:', data.toString());
-                event.sender.send('processing-output', data.toString());
-            });
-
-            process.on('close', (code) => {
-                resolve(`Process exited with code ${code}`);
-            });
-
-            process.on('error', (error) => {
-                reject(`error: ${error.message}`);
-            });
-        }).catch((error) => {
-            console.error('Error creating sketch:', error);
-            reject(error);
-        });
+        // createSketchFile(fileName, content).then((folderPath) => {
+        //     console.log('Sketch created successfully', folderPath);
+        //
+        //     const process = exec(`processing-java --sketch=${folderPath} --run`);
+        //
+        //     process.stdout.on('data', (data) => {
+        //         console.log('stdout:', data.toString());
+        //         event.sender.send('processing-output', data.toString());
+        //     });
+        //
+        //     process.stderr.on('data', (data) => {
+        //         console.error('stderr:', data.toString());
+        //         event.sender.send('processing-output', data.toString());
+        //     });
+        //
+        //     process.on('close', (code) => {
+        //         resolve(`Process exited with code ${code}`);
+        //     });
+        //
+        //     process.on('error', (error) => {
+        //         reject(`error: ${error.message}`);
+        //     });
+        // }).catch((error) => {
+        //     console.error('Error creating sketch:', error);
+        //     reject(error);
+        // });
     });
 });
