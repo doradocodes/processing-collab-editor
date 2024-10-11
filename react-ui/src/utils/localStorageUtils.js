4aa0ce1283371ba72ef58dc177e19c8ab5be2d7e
Dora Do <doradocodes@gmail.com>
2024-10-11 18:03:28 -0400

    Update console logs

--- react-ui/src/utils/localStorageUtils.js
+++ react-ui/src/utils/localStorageUtils.js
@@ -1,5 +1,6 @@
 export const getSketchFolders = async () => {
     const folders = await window.electronAPI.getSketchFolders();
+    console.log('Sketch folders:', folders);
     return folders;
 };
 
@@ -7,6 +8,7 @@ export const updateSketch = async (fileName, content) => {
     try {
         const sanitizedFileName = fileName.replace(/[^a-z0-9]/gi, '_').toLowerCase();
         const folderPath = await window.electronAPI.createNewSketch(sanitizedFileName, content);
+        console.log('Saved sketch:', folderPath);
         return folderPath;
     } catch(error) {
         console.error('Error updating sketch:', error);
@@ -15,7 +17,7 @@ export const updateSketch = async (fileName, content) => {
 
 export const getSketchFile = async (folder) => {
     const fileContent = await window.electronAPI.getSketchFile(folder);
-    console.log('fileContent', fileContent);
+    console.log('File content of ', folder, ':\n', fileContent);
     return fileContent;
 }
 
@@ -23,6 +25,7 @@ export const renameSketch = async (oldName, newName) => {
     try {
         const sanitizedFileName = newName.replace(/[^a-z0-9]/gi, '_').toLowerCase();
         await window.electronAPI.renameSketch(oldName, sanitizedFileName);
+        console.log('Renamed sketch:', oldName, '->', sanitizedFileName);
     } catch(error) {
         console.error('Error renaming sketch:', error);
     }
