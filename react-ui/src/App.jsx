fed9295f5c74bf13d64e77cd7a7a85631afb67a6
Dora Do <doradocodes@gmail.com>
2024-10-11 21:29:04 -0400

    Remove unused import

--- react-ui/src/App.jsx
+++ react-ui/src/App.jsx
@@ -1,13 +1,10 @@
 import React, {useEffect, useState} from "react";
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Main from "./views/Main/index.jsx";
 import './App.css';
 import {Theme} from "@radix-ui/themes";
 
 function App() {
     const [theme, setTheme] = useState('light');
-
-
 
     useEffect(() => {
         window.electronAPI.onSetTheme((newTheme) => {
